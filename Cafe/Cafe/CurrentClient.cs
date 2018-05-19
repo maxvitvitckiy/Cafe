9e09ffb1095c33195f0ad7ad42934cf83d788de2
wispeckiy <max.vitvitckiy@gmail.com>
2018-05-19 20:41:10 +0300

    globalUpdate2.2 Updated CurrentClient

--- Cafe/Cafe/CurrentClient.cs
+++ Cafe/Cafe/CurrentClient.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace Cafe
 {
-    class CurrentClient
+    class CurrentClient:IClient
     {
+        int tryCount;
+        int state;
+
+        public CurrentClient()
+        {
+            tryCount = 3;
+            Random rand = new Random();
+            state = rand.Next(1, 2);
+        }
+
+        public int Pay()
+        {
+            if (state == 0) return -10;
+            else return state * 10;
+        }
     }
 }
